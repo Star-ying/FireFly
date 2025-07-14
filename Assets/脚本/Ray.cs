7800f576e18fce88421eeb24f7fb3ad3e7106c5c
1 <ybkhu4053nrt36@163.com>
2025-07-14 16:03:24 +0800

    流萤大战丘丘人基础版
    Signed-off-by: 1 <ybkhu4053nrt36@163.com>

---
+++ Assets/脚本/Ray.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ray : MonoBehaviour
+{
+    public Vector2 movement;
+    public Vector2 temp;
+    private void OnEnable()
+    {
+        temp = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.Instance.IsPlaying &&
+            GameManager.Exists &&
+            Player.Instance.isSaMu && 
+            (!Player.Instance.isFight ||
+            Player.Instance.L_energy <= 0) &&
+            transform.localScale.y > 0f)
+        {
+            transform.localScale -= new Vector3(0, 1f * Time.deltaTime);
+        }
+        else if(!Player.Instance.isSaMu || transform.localScale.y <= 0)
+        {
+            gameObject.SetActive(false);
+            transform.localScale = temp;
+        }
+    }
+}
